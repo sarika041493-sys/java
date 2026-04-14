0b6f379d18324a89e0ba2e456f03efa190ec47e7
sarika041493-sys <sarika041493@gmail.com>
2026-04-14 21:55:05 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,18 @@
+package day2;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		System.out.println("----Arithemetic operators---");
+		int num1 = 10;
+		int num2 = 40;
+		System.out.println(" Addition" + (num1 + num2));
+		System.out.println(" Subtraction" + (num1 - 12));
+		System.out.println(" Multiplication" + (num1 * num2));
+		System.out.println(" Division" + (num2 / 5));
+		System.out.println(" Modulus" + (num1 % num2));
+		// TODO Auto-generated method stub
+
+	}
+
+}
