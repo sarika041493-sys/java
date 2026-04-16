4501989c7fea4f6d981dc84461e8cbfb69e69374
sarika041493-sys <sarika041493@gmail.com>
2026-04-16 15:48:38 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,33 @@
+package day3;
+
+public class assignment {
+
+	public static void main(String[] args) {
+		System.out.println("----Positivve or negative number---");
+		
+		int num=5;
+		System.out.println("enter a number: " +num);
+		if(num > 0) {
+			System.out.println("the number is positive");
+		}
+		else {
+			System.out.println("number is negative");
+		}
+		
+		System.out.println("---- even or odd----");
+		
+		int a=3;
+		System.out.println("enter a number: " +a);
+		
+		if(a % 2==0) {
+			System.out.println("even");
+			
+		}
+		else {
+			System.out.println("odd");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
