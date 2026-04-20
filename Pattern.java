bb41a2548abba5a1daef8abf903f84d062996ccb
sarika041493-sys <sarika041493@gmail.com>
2026-04-20 10:51:53 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,41 @@
+package day6;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		int n=5;
+		
+		for( int i = 1; i <= n; i ++)
+		{
+			for( int j =1; j <= n; j++)
+				{
+				if( i == 1 || i == n || j ==1 || j ==n)
+				{
+				System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+		}
+			System.out.println();
+		// TODO Auto-generated method stub
+
+	}
+	
+		for( int i =1; i <=n; i++)
+		{
+			for ( int j=1; j<=n-i; j++)
+			{
+				System.out.print(" ");
+			}
+			for( int j=1; j<=2*i -1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+}
+}
